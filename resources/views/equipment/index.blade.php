55d64aedf13eccbe3a25cecd106be8ed9bef64f5
Ivan <ivanablao2@gmail.com>
2025-06-26 16:50:51 +0800

    Added network access

--- resources/views/equipment/index.blade.php
+++ resources/views/equipment/index.blade.php
@@ -14,7 +14,6 @@
             <a href="{{ route('equipment.create') }}" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700">
                 Add New Equipment
             </a>
-
             <!-- ADD CLASSES TO THIS LINK -->
             <a href="{{ route('import.show') }}" class="inline-flex items-center px-4 py-2 bg-blue-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-blue-500">
                 Import from CSV
--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,10 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    server: {
+        host: '0.0.0.0', // This tells Vite to listen on all available network interfaces
+        hmr: {
+            host: 'localhost', // Hot Module Replacement should still point to localhost for your own browser
+        }
+    },
 });
