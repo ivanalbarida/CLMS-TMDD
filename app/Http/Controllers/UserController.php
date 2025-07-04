e838ad41035db9525a836b9cff98e0ae6c60b16d
Ivan <ivanablao2@gmail.com>
2025-07-04 09:57:41 +0800

    fixed edit button on annoucement card and fixed user deletion button on user management page for admins

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
