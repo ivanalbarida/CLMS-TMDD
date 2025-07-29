6d6c9ddddf7814aed13a6a35ea07e15052ff4936
ivanalbarida <ivanablao2@gmail.com>
2025-07-29 15:04:13 +0800

    updates1

--- config/database.php
+++ config/database.php
@@ -16,7 +16,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'sqlite'),
+    'default' => 'mysql',
 
     /*
     |--------------------------------------------------------------------------
@@ -44,15 +44,15 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
+            'url' => env('DATABASE_URL'),
+            'host' => '127.0.0.1', // Hardcode
+            'port' => '3306', // Hardcode
+            'database' => 'clms_db', // Hardcode
+            'username' => 'root', // Hardcode
+            'password' => '', // Hardcode
             'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => env('DB_CHARSET', 'utf8mb4'),
-            'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
             'prefix_indexes' => true,
             'strict' => true,
